1f25168f01d46f131b85ac954cc68d5ef1af65d1
iannico322 <iannicocaulin@gmail.com>
2024-09-25 11:08:41 +0800

    ok

--- src/plugin/axios.tsx
+++ src/plugin/axios.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
  
 
-axios.defaults.baseURL = "https://balaodmindanaw.up.railway.app/"
+axios.defaults.baseURL = "https://balaod.mindanao.06222001.xyz/"
 axios.defaults.headers.get['Accept'] = 'application/json';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
